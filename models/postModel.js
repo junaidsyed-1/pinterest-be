705ce7f7b2803d1f32e21ced27e3b2e177141cc0
junaid <jaanusd79@gmail.com>
2024-03-16 16:58:20 +0530

    Updated

--- models/postModel.js
+++ models/postModel.js
@@ -10,7 +10,7 @@ const postSchema = new mongoose.Schema({
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
     },
     like: {
         type: Array,
